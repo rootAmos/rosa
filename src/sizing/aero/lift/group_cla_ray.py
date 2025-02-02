fixed lift calculation

src/sizing/aero/lift/group_cla_ray.py
@@ -2,144 +2,89 @@ import openmdao.api as om
 import numpy as np
 
 from cl_alpha_airfoil import LiftCurveSlope3D
-class CoupledCLAlphaCanard(om.ExplicitComponent):
+
+class CoupledCLAlphaRay(om.ExplicitComponent):
     """
     Calculates the effective lift curve slope for the canard, accounting for area ratio and downwash.
     
 
     Inputs:
-        CL_alpha_c : float
-            Canard lift curve slope [1/rad]
-        deps_c_da : float
+        CL_alpha : float
+            Ray lift curve slope [1/rad]
+        deps_ray_da : float
             Change in canard downwash angle with respect to angle of attack [-]
-        S_c : float
-            Canard reference area [m^2]
-        S_w : float
-            Wing reference area [m^2]
+        S_ray : float
+            Ray reference area [m^2]
+        S_manta : float
+            Manta reference area [m^2]
     
     Outputs:
-        CL_alpha_c_eff : float
+        CL_alpha_eff : float
             Effective canard lift curve slope [1/rad]
     """
 
     def initialize(self):
         self.options.declare('manta', default=0, desc='Flag for Manta configuration')
-        self.options.declare('ray', default=0, desc='Flag for Ray configuration')
     
 
     def setup(self):
         # Inputs
 
-        self.add_input('CL_alpha_c', val=0.0, units='1/rad',
-                      desc='Canard lift curve slope')
+        self.add_input('CL_alpha', val=0.0, units='1/rad',
+                      desc='Ray lift curve slope')
 
         
         if self.options['manta']:
-            self.add_input('S_c', val=0.0, units='m**2',
-                          desc='Canard reference area')
-            self.add_input('S_w', val=0.0, units='m**2',
-                          desc='Wing reference area')
-            self.add_input('d_eps_c_d_alpha', val=0.0,
-                      desc='Canard downwash derivative')
+            self.add_input('S_ray', val=0.0, units='m**2',
+                          desc='Ray reference area')
+            self.add_input('S_manta', val=0.0, units='m**2',
+                          desc='Manta reference area')
+            self.add_input('d_eps_ray_d_alpha', val=0.0,
+                      desc='Ray downwash derivative')
         # end
 
         
         # Outputs
-        self.add_output('CL_alpha_c_eff', val=0.0, units='1/rad',
+        self.add_output('CL_alpha_eff', val=0.0, units='1/rad',
                        desc='Effective canard lift curve slope')
 
         
         # Partials
-        self.declare_partials('CL_alpha_c_eff', ['*'])
+        self.declare_partials('CL_alpha_eff', ['*'])
         
     def compute(self, inputs, outputs):
-        CL_alpha_c = inputs['CL_alpha_c']
-        d_eps_c_d_alpha = inputs['d_eps_c_d_alpha']
+        CL_alpha = inputs['CL_alpha']
+        d_eps_ray_d_alpha = inputs['d_eps_ray_d_alpha']
 
         if self.options['manta']:
-            S_c = inputs['S_c']
-            S_w = inputs['S_w']
-            area_ratio = S_c / S_w
-            outputs['CL_alpha_c_eff'] = CL_alpha_c  * (1 + d_eps_c_d_alpha) * area_ratio
+            S_ray = inputs['S_ray']
+            S_manta = inputs['S_manta']
+            area_ratio = S_ray / S_manta
+            outputs['CL_alpha_eff'] = CL_alpha  * (1 + d_eps_ray_d_alpha) * area_ratio
         else:
-            outputs['CL_alpha_c_eff'] = CL_alpha_c 
+            outputs['CL_alpha_eff'] = CL_alpha 
         # end
 
         
     def compute_partials(self, inputs, partials):
-        d_eps_c_d_alpha = inputs['d_eps_c_d_alpha']
-        CL_alpha_c = inputs['CL_alpha_c']
+        d_eps_ray_d_alpha = inputs['d_eps_ray_d_alpha']
+        CL_alpha = inputs['CL_alpha']
 
         if self.options['manta'] ==1:
-            S_c = inputs['S_c']
-            S_w = inputs['S_w']
-            area_ratio = S_c / S_w
-            partials['CL_alpha_c_eff', 'CL_alpha_c'] = (1 + d_eps_c_d_alpha) * area_ratio
-            partials['CL_alpha_c_eff', 'd_eps_c_d_alpha'] = CL_alpha_c  * area_ratio
-            partials['CL_alpha_c_eff', 'S_c'] = CL_alpha_c * (1 + d_eps_c_d_alpha) / S_w
-            partials['CL_alpha_c_eff', 'S_w'] = -CL_alpha_c * (1 + d_eps_c_d_alpha) * S_c / S_w**2
+            S_ray = inputs['S_ray']
+            S_manta = inputs['S_manta']
+            area_ratio = S_ray / S_manta
+            partials['CL_alpha_eff', 'CL_alpha'] = (1 + d_eps_ray_d_alpha) * area_ratio
+            partials['CL_alpha_eff', 'd_eps_ray_d_alpha'] = CL_alpha  * area_ratio
+            partials['CL_alpha_eff', 'S_ray'] = CL_alpha * (1 + d_eps_ray_d_alpha) / S_manta
+            partials['CL_alpha_eff', 'S_manta'] = -CL_alpha * (1 + d_eps_ray_d_alpha) * S_ray / S_manta**2
         else:
 
 
-            partials['CL_alpha_c_eff', 'CL_alpha_c'] = 1
+            partials['CL_alpha_eff', 'CL_alpha'] = 1
 
 
-class CoupledCLAlphaWing(om.ExplicitComponent):
-    """
-    Calculates the effective lift curve slope for the wing, accounting for downwash.
-    
-
-    Inputs:
-        CL_alpha_w : float
-            Wing lift curve slope [1/rad]
-        deps_w_da : float
-            Change in wing downwash angle with respect to angle of attack [-]
-    
-    Outputs:
-        CL_alpha_w_eff : float
-            Effective wing lift curve slope [1/rad]
-    """
-
-    def initialize(self):
-        self.options.declare('ray', default=0, desc='Flag for Ray configuration')
-    
-    def setup(self):
-
-        # Inputs
-        self.add_input('CL_alpha_w', val=0.0, units='1/rad', 
-                      desc='Wing lift curve slope')
-        
-        if self.options['ray']:
-            self.add_input('d_eps_w_d_alpha', val=0.0, 
-                      desc='Wing downwash derivative from canard influence')
-        # end
-        
-        # Outputs
-        self.add_output('CL_alpha_w_eff', val=0.0, units='1/rad',
-                       desc='Effective wing lift curve slope')
-        
-        # Partials
-        self.declare_partials('CL_alpha_w_eff', ['*'])
-        
-
-    def compute(self, inputs, outputs):
-        CL_alpha_w = inputs['CL_alpha_w']
-        d_eps_w_d_alpha = inputs['d_eps_w_d_alpha']
-
-        if self.options['ray'] == 1:
-            outputs['CL_alpha_w_eff'] = CL_alpha_w * (1 - d_eps_w_d_alpha)
-        else:
-            outputs['CL_alpha_w_eff'] = CL_alpha_w
-
-        # end
-        
-
-    def compute_partials(self, inputs, partials):
-        partials['CL_alpha_w_eff', 'CL_alpha_w'] = 1 - inputs['d_eps_w_d_alpha']
-        partials['CL_alpha_w_eff', 'd_eps_w_d_alpha'] = -inputs['CL_alpha_w'] 
-
-
-class GroupCLAlpha(om.Group):
+class GroupCLAlphaRay(om.Group):
     """
     Group that computes total lift curve slope by summing contributions
     from wing and canard.
@@ -147,69 +92,24 @@ class GroupCLAlpha(om.Group):
 
     def initialize(self):
         self.options.declare('manta', default=0, desc='Flag for Manta configuration')
-        self.options.declare('ray', default=0, desc='Flag for Ray configuration')
-    
+
     def setup(self):
 
-        scaling_factors = [self.options['manta'], self.options['ray']]
-        # Wing lift curve slope
 
-
-        if self.options['manta'] == 1:
-
-            self.add_subsystem('w_cl_alpha_3d',
-                LiftCurveSlope3D(),
-                promotes_inputs=[],
-                promotes_outputs=[])
-                        
-            self.add_subsystem('wing_cl_alpha', 
-                            CoupledCLAlphaWing(ray=self.options['ray']),
-                            promotes_inputs=['CL_alpha_w',
-                                            'd_eps_w_d_alpha'],
-                            promotes_outputs=['CL_alpha_w_eff'])
+        self.add_subsystem('cl_alpha_3d',
+                            LiftCurveSlope3D(),
+                            promotes_inputs=['*'],
+                            promotes_outputs=['*'])
         
+        # Ray lift curve slope
+        self.add_subsystem('cpld_cl_alpha',
+                        CoupledCLAlphaRay(manta=self.options['manta']),
+                        promotes_inputs=['*'],
+                        promotes_outputs=['*'])
 
-            self.connect('w_cl_alpha_3d.CL_alpha', 'CL_alpha_w')
-        # end
+    # end
 
-
-
-
-        if self.options['ray'] == 1:
-
-            
-            self.add_subsystem('c_cl_alpha_3d',
-                                LiftCurveSlope3D(),
-                                promotes_inputs=[],
-                                promotes_outputs=[])
-            
-                        # Canard lift curve slope
-            self.add_subsystem('canard_cl_alpha',
-                            CoupledCLAlphaCanard(manta=self.options['manta']),
-                            promotes_inputs=['CL_alpha_c',
-                                            'd_eps_c_d_alpha',
-                                            'S_c', 'S_w'],
-                            promotes_outputs=['CL_alpha_c_eff'])
-            
-            self.connect('c_cl_alpha_3d.CL_alpha', 'CL_alpha_c')
-        # end
-
-        
-
-
-        if self.options['manta'] == 1 and self.options['ray'] == 1:
-            # Sum the contributions
-            adder = om.AddSubtractComp()
-            adder.add_equation('CL_alpha_total',
-                            ['CL_alpha_w_eff', 'CL_alpha_c_eff'],
-                            desc='Total lift curve slope',
-                            scaling_factors=scaling_factors,units='1/rad')
-
-
-
-            self.add_subsystem('sum_cl_alpha', adder, promotes=['*'])
-        # end
-
+    
 
 if __name__ == "__main__":
     # Create problem instance
@@ -218,40 +118,24 @@ if __name__ == "__main__":
     # Create IndepVarComp
     ivc = om.IndepVarComp()
     
-    # Wing parameters
-    
-    # Wing parameters
-    ivc.add_output('mach_w', val=0.78, desc='Wing Mach number')
-    ivc.add_output('phi_50_w', val=5.0, units='deg', desc='Wing 50% chord sweep angle')
-    ivc.add_output('cl_alpha_airfoil_w', val=2*np.pi, units='1/rad', desc='Wing airfoil lift curve slope')
-    ivc.add_output('aspect_ratio_w', val=10.0, desc='Wing aspect ratio')
-    ivc.add_output('d_eps_w_d_alpha', val=0.25, desc='Wing downwash derivative')
+    # Ray parameters
+    ivc.add_output('mach', val=0.78, desc='Ray Mach number')
+    ivc.add_output('phi_50', val=4.0, units='deg', desc='Ray 50% chord sweep angle')
+    ivc.add_output('cl_alpha_airfoil', val=2*np.pi, units='1/rad', desc='Ray airfoil lift curve slope')
 
-    # Canard parameters
-    ivc.add_output('mach_c', val=0.78, desc='Canard Mach number')
-    ivc.add_output('phi_50_c', val=4.0, units='deg', desc='Canard 50% chord sweep angle')
-    ivc.add_output('cl_alpha_airfoil_c', val=2*np.pi, units='1/rad', desc='Canard airfoil lift curve slope')
-    ivc.add_output('aspect_ratio_c', val=10.0, desc='Canard aspect ratio')
-    ivc.add_output('d_eps_c_d_alpha', val=0.1, desc='Canard downwash derivative')
+    ivc.add_output('aspect_ratio', val=10.0, desc='Ray aspect ratio')
+    ivc.add_output('d_eps_ray_d_alpha', val=0.1, desc='Ray downwash derivative')
     
-    ivc.add_output('S_c', val=20.0, units='m**2', desc='Canard reference area')
-    ivc.add_output('S_w', val=120.0, units='m**2', desc='Wing reference area')
+    S_ref = 25
+    ivc.add_output('S_ray', val=S_ref, units='m**2', desc='Ray wing area')
+    ivc.add_output('S_manta', val=S_ref, units='m**2', desc='Manta wing area')
     
+
+
     # Add subsystems to model
     prob.model.add_subsystem('inputs', ivc, promotes=['*'])
-    prob.model.add_subsystem('cl_alpha_coupled', GroupCLAlpha(manta=1, ray=1), promotes=['*'])
+    prob.model.add_subsystem('cl_alpha_ray', GroupCLAlphaRay(manta=1), promotes=['*'])
 
-    
-    prob.model.connect('mach_w', 'w_cl_alpha_3d.mach')
-    prob.model.connect('phi_50_w', 'w_cl_alpha_3d.phi_50')
-    prob.model.connect('aspect_ratio_w', 'w_cl_alpha_3d.aspect_ratio')
-    prob.model.connect('cl_alpha_airfoil_w', 'w_cl_alpha_3d.cl_alpha_airfoil')
-
-    prob.model.connect('mach_c', 'c_cl_alpha_3d.mach')
-
-    prob.model.connect('phi_50_c', 'c_cl_alpha_3d.phi_50')
-    prob.model.connect('cl_alpha_airfoil_c', 'c_cl_alpha_3d.cl_alpha_airfoil')
-    prob.model.connect('aspect_ratio_c', 'c_cl_alpha_3d.aspect_ratio')
 
 
 
@@ -268,11 +152,14 @@ if __name__ == "__main__":
     
     print('\nBaseline Configuration:')
     print('----------------------')
-    print('Wing:')
-    print(f'  CL_alpha:            {prob.get_val("CL_alpha_w")[0]:8.3f} /rad')
-    print(f'  Downwash:            {prob.get_val("d_eps_w_d_alpha")[0]:8.3f}')
-    print(f'  Effective CL_alpha:   {prob.get_val("CL_alpha_w_eff")[0]:8.3f} /rad')
+    print('Manta:')
+    print(f'  CL_alpha:            {prob.get_val("CL_alpha")[0]:8.3f} /rad')
+    print(f'  Downwash:            {prob.get_val("d_eps_ray_d_alpha")[0]:8.3f}')
+
+    print(f'  Effective CL_alpha:   {prob.get_val("CL_alpha_eff")[0]:8.3f} /rad')
     
-    print('\nCanard:')
+
+
+    print('\nRay:')
 
     prob.check_partials(compact_print=True)

src/sizing/aero/lift/zero_ang_lift.py
@@ -68,7 +68,7 @@ class ZeroAngleLift(om.ExplicitComponent):
     Calculates the zero-angle lift coefficient.
     
     Inputs:
-        CL_alpha : float
+        CL_alpha_eff : float
             Lift curve slope [1/rad]
         alpha0 : float
             Zero-lift angle of attack [rad]
@@ -80,7 +80,7 @@ class ZeroAngleLift(om.ExplicitComponent):
     
     def setup(self):
         # Inputs
-        self.add_input('CL_alpha', val=0.0, units='1/rad', 
+        self.add_input('CL_alpha_eff', val=0.0, units='1/rad', 
                       desc='Lift curve slope')
         self.add_input('alpha0', val=0.0, units='rad', 
                       desc='Zero-lift angle of attack')
@@ -90,22 +90,22 @@ class ZeroAngleLift(om.ExplicitComponent):
                        desc='Zero-angle lift coefficient')
         
         # Declare partials
-        self.declare_partials('CL0', ['CL_alpha', 'alpha0'])
+        self.declare_partials('CL0', ['CL_alpha_eff', 'alpha0'])
         
     def compute(self, inputs, outputs):
-        CL_alpha = inputs['CL_alpha']
+        CL_alpha_eff = inputs['CL_alpha_eff']
         alpha0 = inputs['alpha0']
         
-        # Equation: CL0 = -CL_alpha * alpha0
-        outputs['CL0'] = -CL_alpha * alpha0
+        # Equation: CL0 = -CL_alpha_eff * alpha0
+        outputs['CL0'] = -CL_alpha_eff * alpha0
         
     def compute_partials(self, inputs, partials):
-        CL_alpha = inputs['CL_alpha']
+        CL_alpha_eff = inputs['CL_alpha_eff']
         alpha0 = inputs['alpha0']
         
         # Derivatives
-        partials['CL0', 'CL_alpha'] = -alpha0
-        partials['CL0', 'alpha0'] = -CL_alpha
+        partials['CL0', 'CL_alpha_eff'] = -alpha0
+        partials['CL0', 'alpha0'] = -CL_alpha_eff
 
 
 if __name__ == "__main__":
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     
     # Create IndepVarComp
     ivc = om.IndepVarComp()
-    ivc.add_output('CL_alpha', val=5.5, units='1/rad', desc='Lift curve slope')
+    ivc.add_output('CL_alpha_eff', val=5.5, units='1/rad', desc='Lift curve slope')
     ivc.add_output('alpha0', val=-2.0, units='deg', desc='Zero-lift angle of attack')
     
     # Add subsystems to model
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     
     print('\nBaseline Configuration:')
     print('----------------------')
-    print(f'  CL_alpha:            {prob.get_val("CL_alpha")[0]:8.3f} /rad')
+    print(f'  CL_alpha_eff:            {prob.get_val("CL_alpha_eff")[0]:8.3f} /rad')
     print(f'  Alpha_0:             {np.degrees(prob.get_val("alpha0")[0]):8.3f} deg')
     print(f'  CL_0:                {prob.get_val("CL0")[0]:8.3f}')
     
@@ -139,23 +139,23 @@ if __name__ == "__main__":
     # Create figure with 2 subplots
     plt.figure(figsize=(12, 5))
     
-    # CL_alpha sweep
-    CL_alpha_range = np.linspace(4.0, 7.0, 50)
+    # CL_alpha_eff sweep
+    CL_alpha_eff_range = np.linspace(4.0, 7.0, 50)
     CL0_values = []
-    for cla in CL_alpha_range:
-        prob.set_val('CL_alpha', cla)
+    for cla in CL_alpha_eff_range:
+        prob.set_val('CL_alpha_eff', cla)
         prob.run_model()
         CL0_values.append(prob.get_val('CL0')[0])
     
     plt.subplot(121)
-    plt.plot(CL_alpha_range, CL0_values)
-    plt.xlabel('CL_alpha [1/rad]')
+    plt.plot(CL_alpha_eff_range, CL0_values)
+    plt.xlabel('CL_alpha_eff [1/rad]')
     plt.ylabel('CL_0')
     plt.grid(True)
     plt.title('Effect of Lift Curve Slope')
     
     # Alpha0 sweep
-    prob.set_val('CL_alpha', 5.5)  # Reset to baseline
+    prob.set_val('CL_alpha_eff', 5.5)  # Reset to baseline
     alpha0_range = np.linspace(-4.0, 0.0, 50)  # degrees
     CL0_values = []
     for a0 in alpha0_range: